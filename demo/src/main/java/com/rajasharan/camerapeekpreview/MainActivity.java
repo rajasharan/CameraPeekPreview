c89e2b10fd53678f4a0fdee47c5c5f641bc07837
Raja Sharan Mamidala <rajasharan@gmail.com>
2015-09-07 16:52:23 -0400

    add new option in settings to hide description text

--- demo/src/main/java/com/rajasharan/camerapeekpreview/MainActivity.java
+++ demo/src/main/java/com/rajasharan/camerapeekpreview/MainActivity.java
@@ -3,12 +3,17 @@ package com.rajasharan.camerapeekpreview;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.rajasharan.camera.CameraPeekPreview;
 
 public class MainActivity extends AppCompatActivity implements CameraPeekPreview.OnPictureTakenListener {
 
+    private TextView mTextview;
     private ImageView mImageView;
     private CameraPeekPreview mRoot;
 
@@ -17,7 +22,9 @@ public class MainActivity extends AppCompatActivity implements CameraPeekPreview
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextview = (TextView) findViewById(R.id.textview);
         mImageView = (ImageView) findViewById(R.id.imageview);
+
         mRoot = (CameraPeekPreview) findViewById(R.id.camera_peek);
         mRoot.setOnPictureTakenListener(this);
     }
@@ -25,5 +32,24 @@ public class MainActivity extends AppCompatActivity implements CameraPeekPreview
     @Override
     public void onPictureTaken(Bitmap bitmap) {
         mImageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_hide:
+                mTextview.setVisibility(View.GONE);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
